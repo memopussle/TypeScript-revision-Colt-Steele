e0548efcab3f510987a8b529bbab70c5a819bafd
thugiang <xanhtham.cuc@gmail.com>
2022-11-20 21:04:23 +1300

    seting up the project shopping list

---
+++ shopping-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeter from "./components/Greeter";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+     <Greeter />
+    </div>
+  );
+}
+
+export default App;

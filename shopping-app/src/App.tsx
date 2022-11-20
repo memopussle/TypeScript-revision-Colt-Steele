0a443d623bb02fa0c8814677f91d96f132c0adb2
thugiang <xanhtham.cuc@gmail.com>
2022-11-20 21:50:26 +1300

    define props with interface

--- shopping-app/src/App.tsx
+++ shopping-app/src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Greeter from "./components/Greeter";
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import ShoppingList from "./components/ShoppingList";
+import Item from "./models/item";
+
 
 const App = () => {
+  const [items, setItems] = useState<Item[]>([]); // this items has type structure of Item
+  //  const items = [
+  //    { id: 1, product: "Lemon", quantity: 3 },
+  //    { id: 2, product: "Chicken Breast", quantity: 2 },
+  //  ];
   return (
-    <div className="App">
-     <Greeter />
+    <div>
+      <ShoppingList items={items} />
     </div>
   );
-}
+};
 
 export default App;
--- shopping-app/src/components/Greeter.tsx
+++ shopping-app/src/components/Greeter.tsx
@@ -1,9 +1,16 @@
-import React from "react"; 
+import React from "react";
 
-
-
-function Greeter() {
-  return <h1>Hello!!</h1>;
+interface GreeterProps {
+  person: string;
 }
+
+function Greeter({person}: GreeterProps): JSX.Element { // destructure person out of props object
+  // explicitly declare JSx.element can imply that this function result is only jsx.element
+  return <h1>Hello, {person}</h1>;
+}
+
+// const Greeter: React.FC = () => {
+//     return <h1>Hello, Jackie</h1>
+// } //this Greeter function is a type of React Function Component
 
 export default Greeter;
